956c0c94084b765fa49785d0da04f8a3607a0a3a
Kishore Chandran <chandrank06@gmail.com>
2024-06-16 23:13:40 +0530

    leg-day initial commit

---
+++ leg-day-ui/src/App.js
@@ -0,0 +1,280 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="bebas-neue-regular">
+      <div class="container m-auto">
+        <header>
+          <nav class="w-full">
+            <div class="flex content-center w-full mt-6">
+              <div class="content-center">
+                <img
+                  class="w-10 h-10 mr-4"
+                  src="./assets/logo.png"
+                  alt="logo"
+                />
+              </div>
+              <div
+                class="text-3xl font-normal text-left content-center"
+                style={{
+                  lineHeight: "38px",
+                  letterSpacing: "0.08em",
+                  textAlign: "left",
+                  color: "#212121",
+                  textWrap: "nowrap",
+                }}
+              >
+                leg day
+              </div>
+              <div
+                class="oswald-font text-sm font-semibold content-center w-full flex"
+                style={{ justifyContent: flex - end, zIndex: 1 }}
+              >
+                <div
+                  class="oswald-font text-sm font-semibold px-4 content-center"
+                  style={{
+                    lineHeight: "26px",
+                    letterSpacing: "4px",
+                    color: "#212121",
+                  }}
+                >
+                  <button
+                    type="button"
+                    class="oswald-font uppercase"
+                    style={{
+                      textAlign: "left",
+                      background: "#3314fe",
+                      padding: "20px 32px 20px 32px",
+                      gap: "10px",
+                      color: "#ffffff",
+                      boxShadow: "1px 1px 5px #212121",
+                    }}
+                  >
+                    For Trainers
+                  </button>
+                </div>
+                <div class="py-2.5 content-center">|</div>
+                <div
+                  class="oswald-font text-sm font-semibold px-4 text-left content-center"
+                  style={{
+                    lineHeight: "26px",
+                    letterSpacing: "4px",
+                    color: "#212121",
+                  }}
+                >
+                  <button
+                    type="button"
+                    class="oswald-font text-sm font-semibold ps-4 text-left content-cente uppercase"
+                    style={{
+                      textAlign: "left",
+                      background:
+                        "linear-gradient(0deg, #f24b4b, #f24b4b) linear-gradient(0deg, #f24b4b, #f24b4b)",
+                      padding: "20px 32px 20px 32px",
+                      gap: "10px",
+                      color: "#ffffff",
+                      boxShadow: "1px 1px 5px #212121",
+                    }}
+                  >
+                    For Athletes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main class="flex">
+          <div class="trasparentParallelogramTop"></div>
+          <div class="orangeStripBig"></div>
+          <div class="greyStripBig">
+            <div
+              class="text-2xl text-white pt-2x"
+              style="transform: rotate(90deg)"
+            >
+              you’re FEARLESS - you’re FEARLESS - you’re FEARLESS - you’re
+              FEARLESS - you’re FEARLESS - you’re FEARLESS - you’re FEARLESS -
+              you’re FEARLESS - you’re FEARLESS - you’re FEARLESS - you’re
+              FEARLESS - you’re FEARLESS - you’re FEARLESS - you’re FEARLESS -
+              you’re FEARLESS - you’re FEARLESS
+            </div>
+          </div>
+
+          <div
+            class="x-Mark"
+            style="right: 10%; top: 55%; transform: rotate(0deg) scale(0.8)"
+          ></div>
+          <div
+            class="x-Mark"
+            style="right: 9%; top: 60%; transform: rotate(-365deg) scale(0.8)"
+          ></div>
+          <div
+            class="x-Mark"
+            style="right: 8%; top: 67%; transform: rotate(0deg) scale(0.8)"
+          ></div>
+
+          <div class="w-1/2 mt-20">
+            <div
+              class="bebas-neue-regular text-8xl font-normal text-left"
+              style="line-height: 130px; color: #212121"
+            >
+              BE SPIRITED FEARLESS AN EVERYDAY ATHLETE
+            </div>
+            <p
+              class="raleway-font py-12"
+              style="
+              font-size: 18px;
+              font-weight: 600;
+              line-height: 30px;
+              letter-spacing: 0.02em;
+              text-align: left;
+              color: #595959;
+              text-transform: capitalize;
+              max-width: 93%;
+            "
+            >
+              Connect with running trainers across wide variety of categories -
+              Get trained at your place,At you convenience,without hassle of
+              equipments
+            </p>
+            <button
+              type="button"
+              class="oswald-font flex"
+              style="
+              text-align: left;
+              background: linear-gradient(0deg, #f24b4b, #f24b4b),
+                linear-gradient(0deg, #f24b4b, #f24b4b);
+              padding: 20px 32px 20px 32px;
+              gap: 10px;
+              color: #ffffff;
+            "
+            >
+              <div
+                class="uppercase"
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "600",
+                  lineHeight: "26px",
+                  letterSpacing: "4px",
+                  textAlign: "left",
+                  textTransform: "uppercase",
+                }}
+              >
+                browse trainers
+              </div>
+              <div>
+                <img
+                  src="./assets/arrow.png"
+                  alt="arrow"
+                  style="width: 0.5rem; height: 0.9rem; margin-top: 75%"
+                />
+              </div>
+            </button>
+            <div
+              class="flex socialSection oswald-font"
+              style="
+              font-size: 14px;
+              font-weight: 500;
+              line-height: 24px;
+              letter-spacing: 5px;
+              text-align: left;
+            "
+            >
+              <div class="uppercase">Follow Us</div>-
+              <div class="facebookLogo mx-"></div>
+              <div class="instagramLogo mx-1"></div>
+              <div class="twitterLogo mx-1"></div>
+            </div>
+            <div class="transparentTextOnLeft uppercase bebas-neue-regular">
+              Fitness
+            </div>
+          </div>
+          <div class="w-1/2">
+            <img
+              class="heroImg"
+              src="./assets/heroimg.png"
+              alt="mainimg"
+              style="width: 30%; position: absolute; top: 17%; right: 13%"
+            />
+          </div>
+        </main>
+        <div style="color: #212121" class="py-12 uppercase">
+          <p
+            class="bebas-neue-regular pb-6"
+            style="
+            font-size: 26px;
+            font-weight: 400;
+            line-height: 31.2px;
+            letter-spacing: 4px;
+            text-align: left;
+          "
+          >
+            what you get
+          </p>
+          <div class="flex">
+            <div
+              class="oswald-font text-base flex"
+              style="
+              font-weight: 600;
+              line-height: 26px;
+              letter-spacing: 2px;
+              text-align: left;
+              align-items: center;
+              width: 15%;
+            "
+            >
+              <img
+                class="mr-4"
+                src="./assets/plan.png"
+                alt=""
+                style="width: 2.5rem; height: 3.5rem"
+              />
+              <p class="">Custom curated fitness plan</p>
+            </div>
+            <div
+              class="oswald-font text-base flex"
+              style="
+              font-weight: 600;
+              line-height: 26px;
+              letter-spacing: 2px;
+              text-align: left;
+              align-items: center;
+              width: 22%;
+            "
+            >
+              <img
+                class="mx-4"
+                src="./assets/strong.png"
+                alt=""
+                style="width: 2.5rem; height: 3.5rem"
+              />
+              <p class="">
+                rehab, crossfit,weight loss, strength training and more
+              </p>
+            </div>
+            <div
+              class="oswald-font text-base flex"
+              style="
+              font-weight: 600;
+              line-height: 26px;
+              letter-spacing: 2px;
+              text-align: left;
+              align-items: center;
+              width: 15%;
+            "
+            >
+              <img
+                class="mx-4"
+                src="./assets/training.png"
+                alt=""
+                style="width: 2.5rem; height: 3.5rem"
+              />
+              <p class="">stellar personal trainers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
